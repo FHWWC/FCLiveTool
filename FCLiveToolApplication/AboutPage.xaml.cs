f16bf64f69fa03dd548695d499eeca758a18bf69
Stella-Lucky-Lee <fruitcompose@163.com>
2023-08-13 11:55:45 +0800

    Add files via upload

--- FCLiveToolApplication/AboutPage.xaml.cs
+++ FCLiveToolApplication/AboutPage.xaml.cs
@@ -13,5 +13,23 @@ public partial class AboutPage : ContentPage
         Navigation.PopAsync();
     }
 
+    private void ContentPage_Loaded(object sender, EventArgs e)
+    {
+        AppVersionText.Text=VersionTracking.CurrentVersion;
+    }
 
+    private void GitHubBtn_Clicked(object sender, EventArgs e)
+    {
+        Launcher.OpenAsync("https://github.com/FHWWC/FCLiveTool");
+    }
+
+    private void EmailBtn_Clicked(object sender, EventArgs e)
+    {
+        Launcher.OpenAsync("mailto:justineedyoumost@163.com");
+    }
+
+    private void GroupsBtn_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("提示信息", "我们的群组：\n", "确定");
+    }
 }
