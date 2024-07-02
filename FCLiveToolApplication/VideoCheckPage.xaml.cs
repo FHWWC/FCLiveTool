3fb70243480b99ac1c0d5721908b634ada22c631
Stella-Lucky-Lee <fruitcompose@163.com>
2024-07-03 01:08:20 +0800

    Add files via upload

--- FCLiveToolApplication/VideoCheckPage.xaml.cs
+++ FCLiveToolApplication/VideoCheckPage.xaml.cs
@@ -6,7 +6,19 @@ public partial class VideoCheckPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void ContentPage_Loaded(object sender, EventArgs e)
+    {
+        InitRegexList();
+    }
+    public void InitRegexList()
+    {
+        List<string> RegexOption = new List<string>() { "规则1", "规则2", "规则3", "规则4", "规则5" };
+        RegexSelectBox.ItemsSource = RegexOption;
+        RegexSelectBox.SelectedIndex=0;
+    }
+
     List<VideoDetailList> CurrentCheckList=new List<VideoDetailList>();
+    string AllVideoData;
     private void M3USourceRBtn_CheckedChanged(object sender, CheckedChangedEventArgs e)
     {
         RadioButton entry = sender as RadioButton;
@@ -23,11 +35,49 @@ public partial class VideoCheckPage : ContentPage
         }
     }
 
-    private void SelectLocalM3UFileBtn_Clicked(object sender, EventArgs e)
+    private async void SelectLocalM3UFileBtn_Clicked(object sender, EventArgs e)
     {
+        int permResult = await new APPPermissions().CheckAndReqPermissions();
+        if (permResult != 0)
+        {
+            await DisplayAlert("提示信息", "请授权读取和写入权限，程序需要读取文件！", "确定");
+            return;
+        }
+
+        var fileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+{
+    { DevicePlatform.iOS, new[] { "com.apple.mpegurl" , "application/vnd.apple.mpegurl" } },
+    { DevicePlatform.macOS, new[] {  "application/vnd.apple.mpegurl" } },
+    { DevicePlatform.Android, new[] { "audio/x-mpegurl"  } },
+    { DevicePlatform.WinUI, new[] { ".m3u"} }
+});
+
+        var filePicker = await FilePicker.PickAsync(new PickOptions()
+        {
+            PickerTitle = "选择M3U文件",
+            FileTypes=fileTypes
+        });
+
+        if (filePicker is not null)
+        {
+            LocalMFileTb.Text=filePicker.FullPath;
+            AllVideoData =File.ReadAllText(filePicker.FullPath);
+
+            LoadDataToCheckList();
+        }
+        else
+        {
+            LocalMFileTb.Text = "已取消选择";
+            await DisplayAlert("提示信息", "您已取消了操作。", "确定");
+        }
 
     }
-
+    public void LoadDataToCheckList()
+    {
+        RegexManager regexManager = new RegexManager();
+        CurrentCheckList=regexManager.DoRegex(AllVideoData, regexManager.GetRegexOptionIndex(RegexOptionCB.IsChecked, (RegexSelectBox.SelectedIndex+1).ToString()));
+        VideoCheckList.ItemsSource= CurrentCheckList;
+    }
     private void M3UAnalysisBtn_Clicked(object sender, EventArgs e)
     {
 
@@ -40,16 +90,33 @@ public partial class VideoCheckPage : ContentPage
 
     private void VideoCheckList_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
     {
+        if (e.PropertyName == "ItemsSource")
+        {
+            VideoCheckList.SelectedItem=null;
 
-    }
-
-    private void RegexSelectBox_SelectedIndexChanged(object sender, EventArgs e)
-    {
-
+            if (VideoCheckList.ItemsSource is not null&&VideoCheckList.ItemsSource.Cast<VideoDetailList>().Count()>0)
+            {
+                VCLIfmText.IsVisible=false;
+            }
+            else
+            {
+                VCLIfmText.IsVisible=true;
+            }
+        }
     }
 
     private void RegexSelectTipBtn_Clicked(object sender, EventArgs e)
     {
+        DisplayAlert("帮助信息", new MsgManager().GetRegexOptionTip(), "关闭");
+    }
 
+    private void RegexSelectBox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if(string.IsNullOrWhiteSpace(AllVideoData))
+        {
+            return;
+        }
+
+        LoadDataToCheckList();
     }
 }
