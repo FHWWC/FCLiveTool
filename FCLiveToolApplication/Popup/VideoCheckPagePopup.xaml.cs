013cbdf8fc8f5d52f0e864d4f894fcbbd6c2031a
Stella-Lucky-Lee <fruitcompose@163.com>
2024-07-13 20:37:55 +0800

    Add files via upload

---
+++ FCLiveToolApplication/Popup/VideoCheckPagePopup.xaml.cs
@@ -0,0 +1,64 @@
+using CommunityToolkit.Maui.Views;
+
+namespace FCLiveToolApplication;
+
+public partial class VideoCheckPagePopup : Popup
+{
+	public VideoCheckPagePopup()
+	{
+		InitializeComponent();
+	}
+
+    private void SaveOptionBtn_Clicked(object sender, EventArgs e)
+    {
+        #region 判断“同时检测的线程数”
+        if (string.IsNullOrWhiteSpace(UseThreadNumTb.Text))
+        {
+            VideoCheckPage.videoCheckPage.PopShowMsg("你还没有输入“同时检测的线程数”！");
+            return;
+        }
+        int threadNum;
+        if (!int.TryParse(UseThreadNumTb.Text, out threadNum))
+        {
+            VideoCheckPage.videoCheckPage.PopShowMsg("“同时检测的线程数”必须是有效的数值！");
+            return;
+        }
+        if (threadNum<1)
+        {
+            threadNum=GlobalParameter.VideoCheckThreadNum;
+        }
+        #endregion
+
+        #region 判断“User-Agent”
+        if (string.IsNullOrWhiteSpace(UseUATb.Text))
+        {
+            VideoCheckPage.videoCheckPage.PopShowMsg("你还没有输入“User-Agent”！");
+            return;
+        }
+        #endregion
+
+        Preferences.Set("VideoCheckThreadNum", threadNum);
+        Preferences.Set("VideoCheckUA", UseUATb.Text);
+
+
+        VideoCheckPage.videoCheckPage.PopShowMsg("已更新检测选项的参数。");
+        this.Close();
+    }
+
+    private async void ResetOptionBtn_Clicked(object sender, EventArgs e)
+    {
+        if (await VideoCheckPage.videoCheckPage.PopShowMsgAndReturn("你要重置所有选项为默认值吗？"))
+        {
+            Preferences.Set("VideoCheckThreadNum", GlobalParameter.VideoCheckThreadNum);
+            Preferences.Set("VideoCheckUA", GlobalParameter.VideoCheckUA);
+
+            this.Close();
+        }
+    }
+
+    private void MainGrid_Loaded(object sender, EventArgs e)
+    {
+        UseThreadNumTb.Text= Preferences.Get("VideoCheckThreadNum", GlobalParameter.VideoCheckThreadNum).ToString();
+        UseUATb.Text= Preferences.Get("VideoCheckUA", GlobalParameter.VideoCheckUA);
+    }
+}
