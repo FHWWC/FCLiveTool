6fd315d9767a19671bbc82b96217b4f2b82eb765
Stella-Lucky-Lee <fruitcompose@163.com>
2025-08-07 16:43:43 +0800

    Add files via upload

--- FCLiveToolApplication/VideoCheckPage.xaml.cs
+++ FCLiveToolApplication/VideoCheckPage.xaml.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Maui.Extensions;
 using CommunityToolkit.Maui.Storage;
 using CommunityToolkit.Maui.Views;
 using FCLiveToolApplication.Popup;
--- FCLiveToolApplication/VideoSubPage.xaml.cs
+++ FCLiveToolApplication/VideoSubPage.xaml.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Maui.Extensions;
 using CommunityToolkit.Maui.Views;
 using FCLiveToolApplication.Popup;
 using System.Xml.Serialization;
